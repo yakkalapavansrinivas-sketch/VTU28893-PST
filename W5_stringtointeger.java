ac8d7e2ce2550b40413a622a22f13c37f3bca381
yakkalapavansrinivas-sketch <yakkalapavansrinivas@gmail.com>
2026-09-11 10:16:07 +0530

    Implement string to integer conversion method

---
+++ W5_stringtointeger.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int myAtoi(String s) {
+
+        int i = 0;
+        int n = s.length();
+
+        // 1. Skip leading spaces
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+
+        // 2. Check sign
+        int sign = 1;
+
+        if (i < n && s.charAt(i) == '-') {
+            sign = -1;
+            i++;
+        } else if (i < n && s.charAt(i) == '+') {
+            i++;
+        }
+
+        // 3. Convert digits
+        int result = 0;
+
+        while (i < n && Character.isDigit(s.charAt(i))) {
+
+            int digit = s.charAt(i) - '0';
+
+            // 4. Check overflow
+            if (result > (Integer.MAX_VALUE - digit) / 10) {
+                if (sign == 1) {
+                    return Integer.MAX_VALUE;
+                } else {
+                    return Integer.MIN_VALUE;
+                }
+            }
+
+            result = result * 10 + digit;
+            i++;
+        }
+
+        return result * sign;
+    }
+}
