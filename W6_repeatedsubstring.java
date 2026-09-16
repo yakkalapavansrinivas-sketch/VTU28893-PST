6736675aa940b87cd02eee11d9408b06797d95c4
yakkalapavansrinivas-sketch <yakkalapavansrinivas@gmail.com>
2026-09-16 10:14:19 +0530

    Implement repeated substring pattern check

---
+++ W6_repeatedsubstring.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        String doubled = s + s;
+
+        return doubled.substring(1, doubled.length() - 1).contains(s);
+    }
+}
